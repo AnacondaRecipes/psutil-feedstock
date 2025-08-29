1712eb6e41150eec06c84c300ac606f65f6dbd23
vchychy <vchychyrko@anaconda.com>
2025-08-29 13:29:04 +0300

    fix rws

--- recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-"%PYTHON%" -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vv
-if errorlevel 1 exit 1
--- recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-if [[ "$target_platform" == osx-* ]]; then
-    export CFLAGS="$CFLAGS -DkIOMainPortDefault=kIOMasterPortDefault"
-fi
-
-$PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vv
